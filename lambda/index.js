e598bd3a4eb5b01b1584b6b542e67739dd5dae42
dev-chulbuji <dev.chulbuji@gmail.com>
2020-03-08 22:46:21 +0900

    feat: divide lambda & kinesis file structure

--- lambda/index.js
+++ lambda/index.js
@@ -1,21 +1,16 @@
 const { Parser } = require('@robojones/nginx-log-parser')
+const schema = `$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"`
 
 exports.handler = function(event, context, callback) {
     event.Records.forEach(record => {
         const payload = Buffer.from(record.kinesis.data, 'base64').toString('ascii');
-		var rowData = ''
+		const jsonData = JSON.parse(payload)
 
-		console.log(`Decoded paylog: ${payload}`)
+		const containerName = jsonData.container_name
+		const accessLog = jsonData.log
 
-		try {
-			rowData = JSON.parse(payload).log
-		} catch (e) {
-			return callback(e)
-		}
-
-		const schema = `$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"`
 		const parser = new Parser(schema)
-		const result = parser.parseLine(rowData)
+		const result = parser.parseLine(accessLog)
 
         console.log('result:', result)
     });
